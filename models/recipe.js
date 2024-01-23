91d830312aafd71288378934f51d3091237a124c
baldwisj <baldwisj@gmail.com>
2024-01-22 20:31:40 -0500

    Created models for all pages/paths

---
+++ models/recipe.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class recipe extends Model { }
+
+recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ingredients:
+        {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        steps:
+        {
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'profile',
+    }
+)
+
+module.exports = Profile;
--- models/users.js
+++ models/users.js
@@ -3,4 +3,61 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 
-// username, email, password ext...
+class users extends Model {
+    checkPassword(loginPass) {
+        return bcrypt.compareSync(loginPass, this.password);
+    };
+};
+
+users.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
+        },
+        email: {
+            type: DaataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        hooks: {
+            beforeCreate: async (newUser) => {
+                newUser.password = await bcrypt.hash(newUser.password, 12);
+                return newUser;
+            },
+            beforeUpdate: async (updateUser) => {
+                updateUser.password = await bcrypt.hash(updatedUser.password, 12);
+            },
+
+        },
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'users',
+    }
+
+);
+
+module.exports = users;
