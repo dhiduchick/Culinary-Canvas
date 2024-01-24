52153decfbe7f7032b42cc22879a08392d9b02db
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-01-23 19:07:55 -0500

    issues fixed with seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,13 @@
 const sequelize = require('../config/connection');
-const{Post, Image, Users} = require('../models');
+const{Post, Users} = require('../models');
 
 const postData = require('./postData.json');
-const imageData = require('./imageData.json');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({force: true});
 
-    const users = await Users.bulkCreate(usersData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    const profiles = await Profile.bulkCreate(profileData, {
+    const users = await Users.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
