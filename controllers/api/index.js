1a13d16a94ecb591c9746f22bcaf3eecc63024e1
baldwisj <baldwisj@gmail.com>
2024-01-28 14:49:52 -0500

    Added google authentication and comments

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,16 @@
+//this reuires each api route and designates the URL route below
 const router = require('express').Router();
 const recipeRoutes = require('./recipeRoutes');
 const userRoutes = require('./userRoutes');
+const authRoute = require('./auth');
+
+
 
 router.use('/recipes', recipeRoutes);
 
 router.use('/users', userRoutes);
+
+router.use('/auth', authRoute)
 
 module.exports = router;
 
